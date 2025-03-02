abb6a0d837ac4963ed2061fc8488a964e4f9ca48
Luis García <luisroberto.gz@outlook.com>
2025-03-02 17:35:24 -0600

    Agregar archivos de proyecto.

---
+++ ESFE.BusinessLogic/DTOs/UserDto.cs
@@ -0,0 +1,19 @@
+﻿using ESFE.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ESFE.BusinessLogic.DTOs;
+
+public class UserResponse
+{
+    public int UserId { get; set; } 
+
+    public string? UserName { get; set; }
+
+    public string? UserNickname { get; set; }
+
+    public string? RolName { get; set; }
+}
---
+++ ESFE.BusinessLogic/UseCases/Quotations/Queries/GetQuotation/GetQuotationHandler.cs
@@ -0,0 +1,22 @@
+﻿using ESFE.BusinessLogic.DTOs;
+using ESFE.DataAccess.Interfaces;
+using ESFE.Entities;
+using Mapster;
+using MediatR;
+
+namespace ESFE.BusinessLogic.UseCases.Quotations.Queries.GetQuotation;
+
+internal sealed class GetQuotationHandler(IEfRepository<Quotation> _repository) : IRequestHandler<GetQuotationQuery, QuotationResponse>
+{
+    public async Task<QuotationResponse> Handle(GetQuotationQuery query, CancellationToken cancellationToken)
+    {
+        var quotation = await _repository.GetByIdAsync(query.quotationId, cancellationToken);
+
+        if (quotation is null)
+        {
+            return new QuotationResponse();
+        }
+
+        return quotation.Adapt<QuotationResponse>();
+    }
+}
