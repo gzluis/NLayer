293797bfbfade89dafb50e170666c27be90bac48
Luis García <luisroberto.gz@outlook.com>
2025-03-08 17:03:23 -0600

    Actualizacion de CRUD y sript

--- ESFE.BusinessLogic/DTOs/UserDto.cs
+++ ESFE.BusinessLogic/DTOs/UserDto.cs
@@ -18,6 +18,37 @@ public class UserResponse
     public string? RolName { get; set; }
 }
 
+public class CreateUserRequest
+{ 
+
+    public int RolId { get; set; }
+
+    public string? UserName { get; set; }
+
+    public string? UserNickname { get; set; }
+
+    public string? UserPassword { get; set; }
+
+    public bool? UserStatus { get; set; }
+
+    public DateOnly? RegistrationDate { get; set; } 
+}
+
+
+public class UpdateUserRequest
+{
+    public int UserId { get; set; }
+
+    public int RolId { get; set; }
+
+    public string? UserName { get; set; }
+
+    public string? UserNickname { get; set; }
+
+    public string? UserPassword { get; set; } 
+}
+
+
 public partial class RoleResponse
 {
     public int RolId { get; set; }
