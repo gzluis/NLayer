0ef8b080569aec02be3442032de9c6053ab5c00d
Marvin antonio Barrera Trigueros <marvin022021@outlook.es>
2025-04-01 17:13:05 -0600

    update cambios

---
+++ ESFE.BusinessLogic/UseCases/Quotations/Specifications/GetQuotationSpec.cs
@@ -0,0 +1,15 @@
+﻿using Ardalis.Specification;
+using ESFE.Entities;
+
+namespace ESFE.BusinessLogic.UseCases.Quotations.Specifications
+{
+    public class GetQuotationSpec : Specification<Quotation>
+    {
+        public GetQuotationSpec(long quotationId)
+        {
+            Query.Where(q => q.QuotationId == quotationId);
+            Query.Include(q => q.QuotationDetails);
+            Query.Include(q => q.User);
+        }
+    }
+}
